48b6f6119c1bd23a1bdb0817297701f81645eb75
kaylacampbell1 <kaylacampbell3003@gmail.com>
2024-01-20 20:35:51 -0500

    fixed migration/seed error

--- src/db/seeds/add-posts.js
+++ src/db/seeds/add-posts.js
@@ -6,8 +6,20 @@ const Post = require('../models/post');
  */
 exports.seed = async (knex) => {
   await Post.deleteAll();
-  await Post.create('1', 'Bloop bloop');
-  await Post.create('1', 'Bloop bloop');
-  await Post.create('1', 'Bloop bloop');
+  await Post.create(
+    "1",
+    "Hey! This is the caption",
+    "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"
+  );
+  await Post.create(
+    "1",
+    "Another caption",
+    "https://www.mytwintiers.com/wp-content/uploads/sites/89/2022/07/Cat.jpg?w=2560&h=1440&crop=1"
+  );
+  await Post.create(
+    "1",
+    "Bloop bloop",
+    "https://blog.wisconsincheeseman.com/wp-content/uploads/sites/10/2022/10/sharp-cheddar-baby-swiss-1-edited-768x697.jpg.webp"
+  );
 
 };
