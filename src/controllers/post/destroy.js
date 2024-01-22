c1b5cce438fd88b3c24b8c9b5bdaabdb3a0a5c93
kaylacampbell1 <kaylacampbell3003@gmail.com>
2024-01-22 17:31:18 -0500

    updated delete logic

---
+++ src/controllers/post/destroy.js
@@ -0,0 +1,11 @@
+const destroy = async (req, res) => {
+  const {
+    db: { Post },
+    params: { id },
+  } = req;
+
+  const posts = await Post.destroy(id);
+  res.send(posts);
+};
+
+module.exports = destroy;
